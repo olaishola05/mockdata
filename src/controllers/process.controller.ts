e9ed16ca3dcb5b63724407aedd45ca2ffced8c1f
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-06-15 16:26:21 +0100

    feat(server): Update & complete process controllers

--- src/controllers/process.controller.ts
+++ src/controllers/process.controller.ts
@@ -1,55 +1,120 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import {Process, PrismaClient } from '@prisma/client';
+import { BadRequestError, NotFoundError, asyncHandler } from '../utils';
+import { errorResponse } from '../middleware';
 
 const processPrismaClient = new PrismaClient();
 
-export const getAllProcesses = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const processes: Process[] = await processPrismaClient.process.findMany({});
+const checkProcessExist = async (processId: string): Promise<Process | null> => {
+  const process: Process | null = await processPrismaClient.process.findUnique({
+    where: {
+      id: processId,
+    },
+    include: {
+      assignee: true,
+    },
+  });
+  return process;
+}
+
+export const getAllProcesses = asyncHandler(async (error: any, req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const processes: Process[] = await processPrismaClient.process.findMany({});
     res.status(200).json({
       status: 'success',
+      message: 'All processes fetched successfully',
       data: processes,
       nHits: processes.length, 
     });
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-export const getProcessById = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const processId = req.params.id;
-    const process: Process | null = await processPrismaClient.process.findUnique({
-      where: {
-        id: processId,
-      },
-    });
+    next(errorResponse(error, req, res, next))
+})
+
+export const getProcessById = asyncHandler(async (error: any, req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const processId = req.params.id;
+    const process = await checkProcessExist(processId);
+    if (!process) {
+      return next(new NotFoundError('process'))
+    }
+    
     res.status(200).json({
       status: 'success',
+      message: 'Process found successfully',
       data: process,
     });
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-export const createProcess = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const processData: Process = req.body;
-    const process: Process = await processPrismaClient.process.create({
+    next(errorResponse(error, req, res, next))
+})
+
+export const createProcess = asyncHandler(async (error: any, req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const processData: Process = req.body;
+
+  const { firstName, lastName, phone, assigneeId } = processData;
+
+  if (!firstName || !lastName || !phone || !assigneeId) {
+    return next(new BadRequestError('Please provide all required fields'))
+  }
+  
+  const process: Process = await processPrismaClient.process.create({
       data: {
         firstName: processData.firstName,
         lastName: processData.lastName,
         phone: processData.phone,
-        assignee: {connect: {id: processData.assigneeId}}
+        assignee: {connect: {id: processData.assigneeId}},
       }
     });
     res.status(201).json({
       status: 'success',
+      message: 'Process created successfully',
       data: process,
     });
-  } catch (error) {
-    console.log(error);
+
+    next(errorResponse(error, req, res, next))
+})
+
+export const updateProcess = asyncHandler(async (error: any, req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const processId = req.params.id;
+  const checkProcess = await checkProcessExist(processId);
+
+  if (!checkProcess) {
+    return next(new NotFoundError('process'))
   }
+  
+  const processData: Process = req.body;
+  const process: Process = await processPrismaClient.process.update({
+      where: {
+        id: processId,
+      },
+      data: {
+        ...processData,
+      }
+    });
+    res.status(201).json({
+      status: 'success',
+      message: 'Process updated successfully',
+      data: process,
+    });
+
+    next(errorResponse(error, req, res, next))
+});
+
+export const deleteProcess = asyncHandler(async (error: any, req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const processId = req.params.id;
+  const checkProcess = await checkProcessExist(processId);
+
+  if (!checkProcess) {
+    return next(new NotFoundError('process'))
+  }
+  
+  const process: Process = await processPrismaClient.process.delete({
+      where: {
+        id: processId,
+      },
+    });
+    res.status(201).json({
+      status: 'success',
+      message: 'Process deleted successfully',
+      data: process,
+    });
+
+    next(errorResponse(error, req, res, next))
 }
