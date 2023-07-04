97bb9d448b6a461327dd897de6c2051a77e7b283
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-07-04 21:03:54 +0100

    feat(app): Remove old task route
    Removed unused task router file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import userRouter from './user.router';
-import taskRouter from './task.router';
 import authRouter from './auth.router';
 import processRouter from './process.router';
 
 export {
   userRouter,
-  taskRouter,
   authRouter,
   processRouter,
 };
